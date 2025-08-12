777171b6c6a31380e4a1aa03154fda4b2cf66cb6
moe <moe@email.com>
2025-08-11 22:51:01 -0500

    created a reducer function

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const employee = { name: 'Harley', age: 22, company: 'Canada', city: 'Toronto' }
+const employee2 = { name: 'Stax', age: 35 }
+
+// const newEmployee = Object.assign({}, employee, {name: "Martin"})
+const newEmployee = {
+  ...employee,
+  name: 'Martin '
+}
+
+newEmployee.company.city = 'Calgary'
+
+const secondEmployee = {
+  ...employee2
+}
+
+console.log(employee)
+console.log(newEmployee)
+console.log(secondEmployee)
