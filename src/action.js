a5e8c46eac6a64ff891bc61c9e78fe29c9ab6b9a
moe <moe@email.com>
2025-08-12 01:32:40 -0500

    solution for exercise

--- src/action.js
+++ src/action.js
@@ -7,3 +7,7 @@ export const addTask = task => {
 export const removeTask = id => {
   return { type: actionTypes.REMOVE_TASK, payload: { id: id } }
 }
+
+const completedTask = id => {
+  return { type: actionTypes.TASK_COMPLETED, payload: { id: id } }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import store from './store'
-import { addTask, removeTask } from './action'
+import { addTask, removeTask, completedTask } from './action'
+
+const unsubscribe = store.subscribe(() => {
+  console.log('Updated', store.getState())
+})
 
 store.dispatch(addTask('Task 1'))
+store.dispatch(addTask('Task 2'))
 console.log(store.getState())
+
+unsubscribe()
+store.dispatch(completedTask(2))
+
 store.dispatch(removeTask(1))
 console.log(store.getState())
 
